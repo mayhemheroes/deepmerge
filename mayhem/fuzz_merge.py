de63eb50364912593e32941e1f7d972c82fb30ee
Bailey Capuano <bcapuano@asu.edu>
2022-11-12 22:55:56 -0500

    Use newer fuzz generator

--- mayhem/fuzz_merge.py
+++ mayhem/fuzz_merge.py
@@ -3,11 +3,11 @@ import atheris
 import logging
 import sys
 
-import deepmerge.exception
-import deepmerge.strategy
 
 with atheris.instrument_imports():
     import deepmerge
+    import deepmerge.exception
+    import deepmerge.strategy
 
 # No logging
 logging.disable(logging.CRITICAL)
@@ -15,88 +15,130 @@ logging.disable(logging.CRITICAL)
 
 @atheris.instrument_func
 def _shuffle_list(l: list, fdp):
-    import random
     """Shuffles a list in place using indices from fdp"""
     for i in reversed(range(1, len(l))):
-        random.shuffle(l, )
         j = fdp.ConsumeIntInRange(0, i)
         l[i], l[j] = l[j], l[i]
+import atheris
+from typing import List, Set, Dict, Tuple, Any
 
 
-@atheris.instrument_func
-def _get_fuzzed_object(fdp: atheris.FuzzedDataProvider, base_name: str, depth: int = 0) -> dict:
-    # Determine if root element should be a list, dict, or set
-    root_ty = fdp.ConsumeIntInRange(0, 2)
-    if root_ty == 0:
-        root = {}
-    elif root_ty == 1:
-        root = []
+def _handle_type(fdp: atheris.FuzzedDataProvider, ty_queue: List[type]) -> Any:
+    """
+    Handles the fuzzing of a single type.
+    :param fdp: FuzzedDataProvider object
+    :param ty_queue: The current stack of types to be used for fuzzing
+    :return: The fuzzed element
+    """
+    if not ty_queue:
+        return None
+    ty = ty_queue.pop(0)
+    if ty is bytes:
+        return fdp.ConsumeBytes(fdp.ConsumeIntInRange(0, 100))
+    elif ty is bytearray:
+        return bytearray(fdp.ConsumeBytes(fdp.ConsumeIntInRange(0, 100)))
+    elif ty is str:
+        return fdp.ConsumeUnicodeNoSurrogates(fdp.ConsumeIntInRange(0, 100))
+    elif ty is float:
+        return fdp.ConsumeRegularFloat()
+    elif ty is bool:
+        return fdp.ConsumeBool()
+    elif ty is int:
+        return fdp.ConsumeInt(4)
+    elif ty is dict:
+        return build_fuzz_dict(fdp, ty_queue)
+    elif ty is list:
+        return build_fuzz_list(fdp, ty_queue)
+    elif ty is set:
+        return build_fuzz_set(fdp, ty_queue)
+    elif ty is tuple:
+        return build_fuzz_tuple(fdp, ty_queue)
     else:
-        root = set()
+        return None
 
-    elem_count = fdp.ConsumeIntInRange(0, 5)
-    try:
-        for i in range(elem_count):
-            # Decide if we want to add a list, dict, frozenset, or concrete value
-            new_val_type = fdp.ConsumeIntInRange(0, 2)
 
-            # To avoid a maximum recursion error, we limit the depth of the fuzzed object
-            if depth > 4:
-                return root
-            if isinstance(root, set):
-                new_val_type = 3  # Force a concrete value
+def build_fuzz_list(fdp: atheris.FuzzedDataProvider, ty_queue: List[type]) -> List[Any]:
+    """
+    Builds a list with fuzzer-defined elements.
+    :param fdp: FuzzedDataProvider object
+    :param ty_queue: The current stack of types to be used for fuzzing
+    :return: The list
+    """
+    if not ty_queue:
+        return []
+    elem_count = fdp.ConsumeIntInRange(1, 5)
+    gen_list = []
 
-            if new_val_type == 0:
-                # Add a new object
-                key_ty = "obj"
-                new_val = _get_fuzzed_object(fdp, base_name, depth + 1)
-            elif new_val_type == 1:
-                # Add a list
-                key_ty = "list"
-                new_val = []
-                for _ in range(fdp.ConsumeIntInRange(0, 2)):
-                    new_val.append(_get_fuzzed_object(fdp, base_name, depth + 1))
-                if isinstance(root, set):
-                    new_val = tuple(new_val)  # List is not hashable, so we convert it to a tuple
-            elif new_val_type == 2:
-                # Add a frozenset
-                key_ty = "set"
-                new_val = []
-                for _ in range(fdp.ConsumeIntInRange(0, 2)):
-                    new_val.append(_get_fuzzed_object(fdp, base_name, depth + 1))
-            else:
-                concrete_ty = fdp.ConsumeIntInRange(0, 5)
-                key_ty = "conc"
-                if concrete_ty == 0:
-                    new_val = fdp.ConsumeInt(8)
-                elif concrete_ty == 1:
-                    new_val = fdp.ConsumeFloat()
-                elif concrete_ty == 2:
-                    new_val = fdp.ConsumeBool()
-                elif concrete_ty == 3:
-                    new_val = fdp.ConsumeUnicode(15)
-                else:
-                    new_val = fdp.ConsumeBytes(15)
+    for _ in range(elem_count):
+        passed_queue = ty_queue.copy()
+        elem = _handle_type(fdp, passed_queue)
+        if elem is not None:
+            gen_list.append(elem)
+    ty_queue.pop(0)  # Pop elem type
 
-            if isinstance(root, dict):
-                key = f"{base_name}_{key_ty}_{depth}_{i}"
-                root[key] = new_val
-            elif isinstance(root, list):
-                root.append(new_val)
-            elif isinstance(root, set):
-                root.add(new_val)
-        return root
-    except RecursionError:
-        return root
+    return gen_list
 
+
+def build_fuzz_set(fdp: atheris.FuzzedDataProvider, ty_queue: List[type]) -> Set[Any]:
+    """
+    Builds a set with fuzzer-defined elements.
+    :param fdp: FuzzedDataProvider object
+    :param ty_queue: The current stack of types to be used for fuzzing
+    :return: The set
+    """
+    if not ty_queue:
+        return set()
+    ty_queue.insert(0, list)
+
+    fuzz_list = _handle_type(fdp, ty_queue)
+    return set(fuzz_list)
+
+
+def build_fuzz_tuple(fdp: atheris.FuzzedDataProvider, ty_queue: List[type]) -> Tuple[Any]:
+    """
+    Builds a tuple with fuzzer-defined elements.
+    :param fdp: FuzzedDataProvider object
+    :param ty_queue: The current stack of types to be used for fuzzing
+    :return: The tuple
+    """
+    if not ty_queue:
+        return tuple()
+    ty_queue.insert(0, list)
+
+    fuzz_list = _handle_type(fdp, ty_queue)
+    return tuple(fuzz_list)
+
+
+def build_fuzz_dict(fdp: atheris.FuzzedDataProvider, ty_queue: List[type]) -> Dict[Any, Any]:
+    """
+    Builds a dictionary with fuzzer-defined keys and values.
+    :param fdp: FuzzedDataProvider object
+    :param ty_queue: The current stack of types to be used for fuzzing
+    :return: The dictionary
+    """
+    if not ty_queue:
+        return {}
+
+    ty_queue.insert(0, list)  # handle key
+    key_list = _handle_type(fdp, ty_queue)
+    ty_queue.insert(0, list)  # handle key
+    val_list = _handle_type(fdp, ty_queue)
+
+    # Shrink lists to match
+    if len(key_list) > len(val_list):
+        key_list = key_list[:len(val_list)]
+    elif len(val_list) > len(key_list):
+        val_list = val_list[:len(key_list)]
+
+    return dict(zip(key_list, val_list))
 
 @atheris.instrument_func
 def TestOneInput(data):
     fdp = atheris.FuzzedDataProvider(data)
     try:
         # Get two fuzzer python objects
-        base = _get_fuzzed_object(fdp, 'a')
-        next = _get_fuzzed_object(fdp, 'b')
+        base = build_fuzz_dict(fdp, [str, list, str])
+        next = build_fuzz_dict(fdp, [str, list, str])
 
         # Shuffle strategies per fuzzer's desire
         dict_strategies = ['merge', 'override']
@@ -121,9 +163,8 @@ def TestOneInput(data):
         
         merger = deepmerge.Merger(type_strategies, fallback_strategies, type_conflict_strategies)
         merger.merge(base, next)
-
-    except deepmerge.exception.DeepMergeException as e:
-        pass
+    except deepmerge.exception.DeepMergeException:
+        return -1
 
 
 def main():
